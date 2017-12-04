11b75aa9d8344c0733ade6ce9eed0a2d21e7cb7f
Andres A <andresazp@gmail.com>
2017-12-04 12:15:21 -0400

    recovery of some things

--- avg_cv/detect_tracks.py
+++ avg_cv/detect_tracks.py
@@ -1,9 +1,11 @@
 # import the necessary packages
 from avg_cv.shape_detector import ShapeDetector
+from scipy.spatial import distance
 import argparse
 import imutils
-import numpy
+import numpy as np
 import cv2
+import pprint
 
 return_tracks = False
 
@@ -20,20 +22,28 @@ return_tracks = False
 #     intersections, roads and avenues"""
 
 ''''condiciones globales sobre las direcciones de las calles y avenidas '''
-def av_right(avenue):
-    if avenue + 1 % 2 > 0:
-        return True
-def st_down(street):
-    if street + 1 % 2 > 0:
-        return True
 
+
+def av_right(avenue):
+    if avenue % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def st_down(street):
+    if street % 2 == 0:
+        return True
+    else:
+        return False
 
 
 class Zone(object):
     """basisc about one intersection un the tacks.
         Vertices must be a list of CV2 points
     """
-    def __init__(self, vertices, center, size, contour = None ):
+
+    def __init__(self, vertices, center, size, contour=None):
         super(Zone, self).__init__()
         self.vertices = vertices
         self.center = center
@@ -41,16 +51,51 @@ class Zone(object):
         self.contour = contour
         self.occupied = False
 
-    def __str__(self):
-        return "Vertices: " + str(self.vertices) + "\nCenter: " + str(self.center)
 
+    def order_vertices(self):
+        pts = self.vertices
+        pprint.pprint(pts)
+        for p in pts:
+            p = p[0][1]
+        pprint.pprint(pts)
+
+
+        # source =  https://www.pyimagesearch.com/2016/03/21/ordering-coordinates-clockwise-with-python-and-opencv/
+        # sort the points based on their x-coordinates
+        xSorted = pts[np.argsort(pts[:, 0]), :]
+
+        # grab the left-most and right-most points from the sorted
+        # x-roodinate points
+        leftMost = xSorted[:2, :]
+        rightMost = xSorted[2:, :]
+
+        # now, sort the left-most coordinates according to their
+        # y-coordinates so we can grab the top-left and bottom-left
+        # points, respectively
+        leftMost = leftMost[np.argsort(leftMost[:, 1]), :]
+        (tl, bl) = leftMost
+
+        # now that we have the top-left coordinate, use it as an
+        # anchor to calculate the Euclidean distance between the
+        # top-left and right-most points; by the Pythagorean
+        # theorem, the point with the largest distance will be
+        # our bottom-right point
+        D = distance.cdist(tl[np.newaxis], rightMost, "euclidean")[0]
+        (br, tr) = rightMost[np.argsort(D)[::-1], :]
+
+        # return the coordinates in top-left, top-right,
+        # bottom-right, and bottom-left order
+        return np.array([tl, tr, br, bl], dtype="float32")
+
+    def __str__(self):
+        return "Vertices: %s \nCenter: %s" % (self.vertices, self.center)
 
 class Crossing(Zone):
     """basisc about one intersection un the tacks.
         Vertices must be a list of CV2 points
     """
-    def __init__(self, av, st, vertices, contour, center, size):
-        super(Crossing, self).__init__(vertices, center, size, contour)
+    def __init__(self, av, st, zone):
+        super(Crossing, self).__init__(zone.vertices, zone.center, zone.size, zone.contour)
         self.av = av
         self.st = st
 
@@ -58,38 +103,99 @@ class Crossing(Zone):
         return super().__str__() + "; Av: " + self.av + ", St :" + self.st
 
     def right(self):
-        return av_right(self.row)
+        return av_right(self.av)
 
     def down(self):
-        return st_down(self.column)
-
-    def vertices_x(self):
-        return sorted(self.vertices, key=lambda vertex: vertex[0][0])
-    def vertices_y(self):
-        return sorted(self.vertices, key=lambda vertex: vertex[0][1])
+        return st_down(self.st)
 
 
-class Intersection(object):
+class Intersection(Crossing):
     """
+    The complete intersection object modeled as a crossing wit two waiting
+    areas, one wating in from the avenue one form the street
     """
-    def __init__(self, intersection, wait_area_avenue, wait_area_street):
-        super(Intersection, self).__init__()
-        self.intersection = intersection
-        self.avenue = intersection.row
-        self.street = intersection.column
-        self.size = intersection.size
+
+    def __init__(self, crossing, wait_area_avenue, wait_area_street):
+        super(Intersection, self).__init__(
+            crossing.av, crossing.st, crossing.zone)
         self.avenue_position = wait_area_avenue
         self.street_position = wait_area_street
-        self.recently_occupied = False
-
-    def right(self):
-        return av_right(self.avenue)
-    def down(self):
-        return av_right(self.street)
 
 
+def from_st(crossing, intersection_zone_list, streets, avenues):
+    """ takes a intersection and intersection list and returns the
+    interection connected from its street
+    only implemented for case down(0) right(0) """
+    av = crossing.av
+    st = crossing.st
+    if crossing.down():
+        intended_from_st_av = av + 1
+        intended_from_st_st = st
+    else:
+        intended_from_st_av = av - 1
+        intended_from_st_st = st
+
+    if intended_from_st_av < 0:
+        intended_from_st_av = 0
+        intended_from_st_st += 1
+
+        if intended_from_st_st > (streets - 1):
+            # top left corner in case down(0) right(0)
+            intended_from_st_st = st
+
+    elif intended_from_st_av > (avenues - 1):
+        intended_from_st_av = av
+        intended_from_st_st += 1
+
+    try:
+        from_st = next(
+            zone for zone in intersection_zone_list
+            if zone.av == intended_from_st_av
+            and zone.st == intended_from_st_st
+        )
+    except StopIteration:
+        raise ValueError(
+            'Se trata de encontrar from_st que no existe',
+            crossing, av, st)
+    return from_st
 
 
+def from_av(crossing, intersection_zone_list, streets, avenues):
+    """ takes a intersection and intersection list and returns the
+    interection connected from its avenue
+    only implemented for case down(0) right(0) """
+    av = crossing.av
+    st = crossing.st
+    if crossing.right():
+        intended_from_av_av = av
+        intended_from_av_st = st - 1
+    else:
+        intended_from_av_av = av
+        intended_from_av_st = st + 1
+
+    if intended_from_av_st < 0:
+        intended_from_av_st = st
+        intended_from_av_av -= 1
+
+        if intended_from_av_st < 0:
+            # top left corner in case down(0) right(0)
+            intended_from_av_av = av
+
+    elif intended_from_av_st > (avenues - 1):
+        intended_from_av_st = st
+        intended_from_av_av += 1
+
+    try:
+        from_av = next(
+            zone for zone in intersection_zone_list
+            if zone.av == intended_from_av_av
+            and zone.st == intended_from_av_st
+        )
+    except StopIteration:
+        raise ValueError(
+            'Se trata de encontrar from_av que no existe',
+            crossing, av, st)
+    return from_av
 
 
 def locate_intersections(frame, avenues, streets):
@@ -111,20 +217,18 @@ def locate_intersections(frame, avenues, streets):
 
     # Display an image
     cv2.imshow("Imagen a procesar", frame)
-    cv2.waitKey(0)
+    cv2.waitKey(800)
+    cv2.destroyWindow("Imagen a procesar")
 
-    # convert the resized image to grayscale, blur it slightly,
-    # and threshold it
+    #preprocess to get iniital contours
     gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
     thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
     # cv2.imshow("threshhold", thresh)
     # cv2.waitKey(0)
 
-    # find contours in the thresholded image and initialize the
-    # shape detector
+    # find contours in the image and initialize the shape detector
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                                 cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if imutils.is_cv2() else contours[1]
@@ -133,7 +237,7 @@ def locate_intersections(frame, avenues, streets):
     # loop over the contours
     i = 0
     display = frame
-    intersection_zone_list = list()
+    intersection_zones = list()
 
     # get all intersections
     for c in contours:
@@ -163,24 +267,25 @@ def locate_intersections(frame, avenues, streets):
             c = c.astype("float")
             c *= ratio
             c = c.astype("int")
-            # print(str(vertices))
 
             vertices = vertices.astype("float")
             vertices *= ratio
             vertices = vertices.astype("int")
 
             item = Zone(vertices, pos, h, contour=c)
-            intersection_zone_list.append(item)
+            intersection_zones.append(item)
             i += 1
     # order by y axis
-    intersection_zone_list.sort(key=lambda zone: zone.center[1], reverse=True)
+    intersection_zones.sort(key=lambda zone: zone.center[1], reverse=True)
     # brake into rows (avenues) to sort by X each
-    rows_zones = [intersection_zone_list[i:i + streets] for i in xrange(0, len(intersection_zone_list), streets)]
+    rows_zones = [intersection_zones[i:i + streets] for i in xrange(0, len(intersection_zones), streets)]
     intersection_zones_ordered = list()
+    # sort on X on each row (av)
     for row in rows_zones:
         row.sort(key=lambda zone: zone.center[0],)
         intersection_zones_ordered += row
-        print(intersection_zones_ordered)
+    crossings = list()
+    #draw and id each crossing
     for i, zone in enumerate(intersection_zones_ordered):
         av = ( (i ) / streets )
         st = ( (i ) % streets )
@@ -196,20 +301,15 @@ def locate_intersections(frame, avenues, streets):
                             (0, 0, 0),
                             2
         )
-        
+        item = Crossing(av, st, zone)
+        crossings.append(item)
 
     cv2.imshow("Intersections", display)
     cv2.waitKey(0)
-    print(str(intersection_zone_list[1].vertices))
-    print(str(intersection_zone_list[1].vertices_x()))
     # show the output image
-    cv2.imshow("Intersecciones", frame)
-    cv2.waitKey(0)
 
     cv2.destroyAllWindows()
-    # for intersection in intersection_list:
-    #     print(str(intersection.vertices))
-    return intersection_zone_list
+    return crossings
 
 
 def define_tracks(capture, avenues, streets):
@@ -221,24 +321,18 @@ def define_tracks(capture, avenues, streets):
     # TODO: alow for dynamic asigment of up or down streets
 
     frame = imutils.resize(capture, width=900)
-    intersection_zone_list = locate_intersections(frame, avenues, streets)
+    crossings = locate_intersections(frame, avenues, streets)
     intersection_list = list()
 
-    for index, crossing in enumerate(intersection_zone_list):
+    for index, crossing in enumerate(crossings):
         # TODO implement better av st resolution
-        av = crossing.row % (avenues - 1)
-        st = crossing.column % (streets - 1)
+        av = crossing.av % (avenues - 1)
+        st = crossing.st % (streets - 1)
         # TODO is this needed at all??
 
         Xmax = frame.shape[0]
         Ymax = frame.shape[1]
 
-        size_ratio = 1.1
-        corner_size_ratio = 3
-        edge_wait_offset = crossing.size * 2
-
-        from_av = None
-        from_st = None
 
         if st_down(0) and av_right(0):
             # other cases not implemented at this time!
@@ -253,87 +347,13 @@ def define_tracks(capture, avenues, streets):
             wait_st_c = [0, 0]
             wait_st = []
 
-            # TODO Create Poligon
+            # general case
 
-                # TODO normal case
+            pprint.pprint(crossing.vertices)
+            pprint.pprint(crossing.order_vertices())
 
-                    # TODO pick the two points most in the iverse direction of traffic
-                        # TODO pick the two points most in sirection of traffic from_st or from_av
-                # TODO edge cases
-                    #todo, if self, taje the two most in the inv direccion of traffic and duplicate make new ones to the edge_wait_offset
-                    # if not self pick the twho most relevant points of each space, ande of the extremes create the copies to the edges
-                # TODO from_av or from st are those 4 or 6 points
-                # TODO calculate center
 
-            #funtions
-            def from_st(crossing, intersection_zone_list, streets, avenues):
-                """ takes a intersection and intersection list and returns the
-                interection connected from its street
-                only implemented for case down(0) right(0) """
-                if crossing.down():
-                    intended_from_st_av = av + 1
-                    intended_from_st_st = st
-                else:
-                    intended_from_st_av = av - 1
-                    intended_from_st_st = st
 
-                if intended_from_st_av < 0:
-                    intended_from_st_av = 0
-                    intended_from_st_st += 1
 
-                    if intended_from_st_st > (streets - 1):
-                        # top left corner in case down(0) right(0)
-                        intended_from_st_st = st
-
-                elif intended_from_st_av > (avenues - 1):
-                    intended_from_st_av = av
-                    intended_from_st_st += 1
-
-                try:
-                    from_st = next(
-                        zone for zone in intersection_zone_list
-                        if zone.row == intended_from_st_av
-                        and zone.column == intended_from_st_st
-                    )
-                except StopIteration:
-                    raise ValueError(
-                        'Se trata de encontrar from_st que no existe',
-                        crossing, av, st)
-                return from_st
-
-            def from_av(crossing, intersection_zone_list, streets, avenues):
-                """ takes a intersection and intersection list and returns the
-                interection connected from its avenue
-                only implemented for case down(0) right(0) """
-                if crossing.right():
-                    intended_from_av_av = av
-                    intended_from_av_st = st - 1
-                else:
-                    intended_from_av_av = av
-                    intended_from_av_st = st + 1
-
-                if intended_from_av_st < 0:
-                    intended_from_av_st = st
-                    intended_from_av_av -= 1
-
-                    if intended_from_av_st < 0:
-                        # top left corner in case down(0) right(0)
-                        intended_from_av_av = av
-
-                elif intended_from_av_st > (avenues - 1):
-                    intended_from_av_st = st
-                    intended_from_av_av += 1
-
-                try:
-                    from_av = next(
-                        zone for zone in intersection_zone_list
-                        if zone.row == intended_from_av_av
-                        and zone.column == intended_from_av_st
-                    )
-                except StopIteration:
-                    raise ValueError(
-                        'Se trata de encontrar from_av que no existe',
-                        crossing, av, st)
-                return from_av
         else:
             raise ValueError('non-implemented case for track directions')
